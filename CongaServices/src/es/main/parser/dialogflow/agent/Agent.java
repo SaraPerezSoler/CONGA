5829d33503a0d593474c7d5d3e5aabbe5a5646d2
Sara Perez Soler <sara.perezs@uam.es>
2022-03-18 12:26:34 +0100

    Fix save empty textAction

--- CongaServices/src/es/main/parser/dialogflow/agent/Agent.java
+++ CongaServices/src/es/main/parser/dialogflow/agent/Agent.java
@@ -276,7 +276,8 @@ public class Agent extends Chatbot {
 		if (botInteraction.getActions().isEmpty()) {
 			Action empty = Message.emptyAction(intent.getBotIntent(bot), bot);
 			saveAction(empty, bot);
-			botInteraction.getActions().add(empty);
+			Action aux = getAction(empty);
+			botInteraction.getActions().add(aux);
 		}
 		interaction.setTarget(botInteraction);
 
